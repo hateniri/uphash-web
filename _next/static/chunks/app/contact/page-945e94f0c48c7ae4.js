5e5f28a1c9320361fade31d247d12f3762baed10
hateniri <shota.imai.personal@gmail.com>
2025-06-24 15:07:07 +0900

    Updates

---
+++ _next/static/chunks/app/contact/page-945e94f0c48c7ae4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[977],{6734:(e,s,_)=>{Promise.resolve().then(_.bind(_,9023))}},e=>{var s=s=>e(e.s=s);e.O(0,[23,441,684,358],()=>s(6734)),_N_E=e.O()}]);
